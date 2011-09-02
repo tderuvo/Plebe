a4896c235ee685316b697068153a7ee3ea8d3a0f
tony deruvo <tonyderuvo@mac.com>
2011-09-02 02:26:55 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Plebe::Application.config.secret_token = 'fe25dccba972e6e79aef4a368b200fbe3c5fa3ce1f59781fbee078c97df968e49625bb480a70ed24a376d35aa70ebb9c1d744af529bcefe04b4dad335ce2aca2'
