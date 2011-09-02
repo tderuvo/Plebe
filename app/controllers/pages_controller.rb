6d5ec48100ed4800e8ec8d9c9f6a7482d28afaa8
tony deruvo <tonyderuvo@mac.com>
2011-09-02 17:50:28 -0400

    Added static pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,23 +1,30 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def help
+    @title = "Help"
   end
 
   def profile
+    @title = "Profile"
   end
 
   def signout
+    @title = "Sign out"
   end
 
   def about
+    @title = "About"
   end
 
   def support
+    @title = "Support"
   end
 
   def developers
+    @title = "Developers"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,18 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title",
+                                    :content => "Plebe | Home")
     end
   end
 
@@ -14,12 +21,24 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                   :content => "Plebe | Help")
+    end
   end
 
   describe "GET 'profile'" do
     it "should be successful" do
       get 'profile'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'profile'
+      response.should have_selector("title",
+                                   :content => "Plebe | Profile")
     end
   end
 
@@ -28,12 +47,24 @@ describe PagesController do
       get 'signout'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'signout'
+      response.should have_selector("title",
+                                   :content => "Plebe | Sign out")
+    end
   end
 
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
+    end
+    
+      it "should have the right title" do
+        get 'about'
+        response.should have_selector("title",
+                                     :content => "Plebe | About")
     end
   end
 
@@ -42,6 +73,12 @@ describe PagesController do
       get 'support'
       response.should be_success
     end
+
+   it "should have the right title" do
+      get 'support'
+      response.should have_selector("title",
+                                   :content => "Plebe | Support")
+    end    
   end
 
   describe "GET 'developers'" do
@@ -49,6 +86,12 @@ describe PagesController do
       get 'developers'
       response.should be_success
     end
+    
+   it "should have the right title" do
+      get 'developers'
+      response.should have_selector("title",
+                                   :content => "Plebe | Developers")
+    end    
   end
 
 end
